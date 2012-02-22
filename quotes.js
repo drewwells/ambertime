ad1e463a0c2b4942ac91a86ae232b0ee5493cf85
Drew Wells <drew.wells00@gmail.com>
2012-02-21 22:08:03 -0600

    Update quotes.js

--- quotes.js
+++ quotes.js
@@ -1,2 +1,2 @@
 var quotes = [];
-quotes.push({ message = 'Ky = greatest ever', "Wed, 22 Feb 2012 04:07:22 GMT"});
+quotes.push({ message = 'Ky = greatest ever', ts = "Wed, 22 Feb 2012 04:07:22 GMT"});
